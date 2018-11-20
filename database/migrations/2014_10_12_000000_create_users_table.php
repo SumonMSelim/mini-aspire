a59a8a8cfcc3228bbdaaf0e9034e844093c1ae56
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2018-11-20 16:18:26 +0600

    users table migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('full_name', 128);
+            $table->string('email', 128)->unique();
+            $table->string('password', 128);
+            $table->string('signup_role', 64)->nullable();
+            $table->string('signup_looking_for', 128)->nullable();
+            $table->string('heard_through', 64)->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
