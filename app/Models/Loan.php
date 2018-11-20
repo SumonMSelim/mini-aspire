8c582080e5fae1d448457fb596eed57e04c9309a
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2018-11-21 02:54:44 +0600

    Loan model added

---
+++ app/Models/Loan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Loan extends Model
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
