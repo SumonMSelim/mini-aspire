097848b86f0f464d6620763f0e9a949e17051a19
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2018-11-22 00:34:45 +0600

    fixed data type issues on loans table for some columns

--- database/migrations/2018_11_20_202422_create_loans_table.php
+++ database/migrations/2018_11_20_202422_create_loans_table.php
@@ -17,12 +17,12 @@ class CreateLoansTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->decimal('requested_amount', 8, 2)->comment('Between SGD$5,000 to SGD$100,000');
-            $table->string('loan_tenor', 16)->comment('1 month to 6 months');
+            $table->tinyInteger('loan_tenor')->comment('unit: month');
             $table->string('repayment_frequency', 16)->default('Monthly');
-            $table->integer('origination_fee_percentage')->comment('one-time ranging from 1% to 6% fee');
-            $table->integer('interest_rate')->comment('Between 1.5% to 4% per month');
+            $table->decimal('origination_fee_percentage', 3, 2)->comment('one-time ranging from 1% to 6% fee');
+            $table->decimal('interest_rate', 3, 2)->comment('Between 1.5% to 4% per month');
             $table->decimal('disbursed_amount', 8, 2)->comment('requested amount - origination fee');
-            $table->string('status', 16)->default('approved');
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
