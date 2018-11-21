daac51df2c43c9734009e6139965f38dd20d2bf2
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2018-11-22 00:44:48 +0600

    currency added

--- database/migrations/2018_11_20_202422_create_loans_table.php
+++ database/migrations/2018_11_20_202422_create_loans_table.php
@@ -16,12 +16,13 @@ class CreateLoansTable extends Migration
         Schema::create('loans', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
-            $table->decimal('requested_amount', 8, 2)->comment('Between SGD$5,000 to SGD$100,000');
-            $table->tinyInteger('loan_tenor')->comment('unit: month');
+            $table->decimal('requested_amount', 11, 2);
+            $table->string('currency', 8)->default('SGD')->comment('');
+            $table->tinyInteger('loan_tenor')->comment('Unit: month');
             $table->string('repayment_frequency', 16)->default('Monthly');
             $table->decimal('origination_fee_percentage', 3, 2)->comment('one-time ranging from 1% to 6% fee');
             $table->decimal('interest_rate', 3, 2)->comment('Between 1.5% to 4% per month');
-            $table->decimal('disbursed_amount', 8, 2)->comment('requested amount - origination fee');
+            $table->decimal('disbursed_amount', 11, 2)->comment('requested amount - origination fee');
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
